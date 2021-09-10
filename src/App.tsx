86585f0cf877d9ee8a12336da0064f77fb1150f6
lena forti <lena.forti@hulu.com>
2021-09-09 21:49:23 -0700

    update firebase project

--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,44 @@
-import React from 'react';
-import './App.css';
-import firebase from 'firebase';
-import withFirebaseAuth, { WrappedComponentProps } from 'react-with-firebase-auth';
-import { Layout } from 'antd';
-import LoggedIn from './components/LoggedIn';
-import TopNav from './components/TopNav';
-import LoggedOut from './components/LoggedOut';
+import React from "react";
+import "./App.css";
+import firebase from "firebase";
+import withFirebaseAuth, {
+  WrappedComponentProps,
+} from "react-with-firebase-auth";
+import { Layout } from "antd";
+import LoggedIn from "./components/LoggedIn";
+import TopNav from "./components/TopNav";
+import LoggedOut from "./components/LoggedOut";
 
 const firebaseApp = firebase.initializeApp({
-  apiKey: "AIzaSyDjZnEbjldaCbHizaFib3051sOiCstgCbs",
-  authDomain: "todolist-7787d.firebaseapp.com",
-  projectId: "todolist-7787d",
-  storageBucket: "todolist-7787d.appspot.com",
-  messagingSenderId: "47076179767",
-  databaseURL: "https://todolist-7787d-default-rtdb.firebaseio.com/",
-  appId: "1:47076179767:web:800d22e52f631d52c8a4b2"
+  apiKey: "AIzaSyBy3Y5w0kfsAp12rfG_XXA6CHhe80TjYrg",
+  authDomain: "food-delivery-f8fe1.firebaseapp.com",
+  projectId: "food-delivery-f8fe1",
+  storageBucket: "food-delivery-f8fe1.appspot.com",
+  messagingSenderId: "453622329220",
+  appId: "1:453622329220:web:ca118c3225633b824337d1",
 });
 
 export const firebaseAppAuth = firebaseApp.auth();
-export const providers = { googleProvider: new firebase.auth.GoogleAuthProvider() };
+export const providers = {
+  googleProvider: new firebase.auth.GoogleAuthProvider(),
+};
 
-
-const App: React.ComponentType<object & WrappedComponentProps> = ({ user, signOut, signInWithGoogle }) =>
+const App: React.ComponentType<object & WrappedComponentProps> = ({
+  user,
+  signOut,
+  signInWithGoogle,
+}) => (
   <div className="App">
     <TopNav user={user} signOut={signOut} />
-    <Layout.Content className='h-screen p-8'>
-      {user ?
-        <LoggedIn user={user} /> :
+    <Layout.Content className="h-screen p-8">
+      {user ? (
+        <LoggedIn user={user} />
+      ) : (
         <LoggedOut signInWithGoogle={signInWithGoogle} />
-      }
+      )}
     </Layout.Content>
-  </div >
-
+  </div>
+);
 
 export default withFirebaseAuth({
   providers,
